ef0fca2d5afb2094ae877231699f50551c18702d
Jeff Shumate <jeff@catapulsion.net>
2026-04-20 11:51:29 -0700

    Add MilestoneKitDemo host app (stock Xcode multiplatform template)

---
+++ MilestoneKitDemo/MilestoneKitDemoTests/MilestoneKitDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  MilestoneKitDemoTests.swift
+//  MilestoneKitDemoTests
+//
+//  Created by Jeff Shumate on 4/20/26.
+//
+
+import Testing
+
+struct MilestoneKitDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
